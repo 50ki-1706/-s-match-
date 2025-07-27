124f9e9506e5527d1ab29adbe977e2c52548ee96
tiyo5151 <s1F102400224@iniad.org>
2025-07-27 12:32:16 +0900

    fix:vercel error

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,11 +1,5 @@
-// import { auth } from '@/auth';
-// import ProfilePage from '@/components/profile/profile';
+const page = () => {
+  return <div>page</div>;
+};
 
-// export default async function Server() {
-//   const session = await auth();
-//   const userId = session?.user?.id;
-
-//   const supabaseUrl = process.env.SUPABASE_URL || '';
-//   const supabaseKey = process.env.SUPABASE_KEY || '';
-//   return <ProfilePage userId={userId} supabaseUrl={supabaseUrl} supabaseKey={supabaseKey} />;
-// }
+export default page;
